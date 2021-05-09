98fded6005788236a796c805c109a75de5ed01c4
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-05-09 23:09:00 +0800

    fix booting
    Change-Id: I086fa8c3d70f7cec3b34fb9e5e432c8eee88ff7e

--- init/util.cpp
+++ init/util.cpp
@@ -236,7 +236,8 @@ int wait_for_file(const char* filename, std::chrono::nanoseconds timeout) {
 
 void ImportKernelCmdline(const std::function<void(const std::string&, const std::string&)>& fn) {
     std::string cmdline;
-    android::base::ReadFileToString("/proc/cmdline", &cmdline);
+    android::base::ReadFileToString("/proc/self/cmdline", &cmdline); // HACKED
+    std::replace(cmdline.begin(), cmdline.end(), '\0', ' '); // HACKED
 
     for (const auto& entry : android::base::Split(android::base::Trim(cmdline), " ")) {
         std::vector<std::string> pieces = android::base::Split(entry, "=");
