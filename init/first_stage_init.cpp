2e04e1034d1c49edfcdcc2bdb172ccdcb36b2092
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-12-17 22:44:33 +0800

    ignore devfs mount

--- init/first_stage_init.cpp
+++ init/first_stage_init.cpp
@@ -235,7 +235,7 @@ int FirstStageMain(int argc, char** argv) {
     CHECKCALL(setenv("PATH", _PATH_DEFPATH, 1));
     // Get the basic filesystem setup we need put together in the initramdisk
     // on / and then we'll let the rc file figure out the rest.
-    CHECKCALL(mount("tmpfs", "/dev", "tmpfs", MS_NOSUID, "mode=0755"));
+    //CHECKCALL(mount("tmpfs", "/dev", "tmpfs", MS_NOSUID, "mode=0755"));
     CHECKCALL(mkdir("/dev/pts", 0755));
     CHECKCALL(mkdir("/dev/socket", 0755));
     CHECKCALL(mkdir("/dev/dm-user", 0755));
